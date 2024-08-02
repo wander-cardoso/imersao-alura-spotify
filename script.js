2347092681087beaca3c0d70ff75c9097eef2c84
Wander-Cardoso <wander.hsmc@gmail.com>
2024-08-02 01:56:34 +0100

    alteracao de algumas palavras no javascript

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const searchInput = document.getElementById('search-input');
 const resultArtists = document.getElementById('result-artists');
-const resultPlaylist = document.getElementById('result-playlist');
+const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = 'http://localhost:8000/artists?name_like=${searchTerm}'
+    const url = `http://localhost:8000/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
         //.then((result) => displayResults(result))
-
 }
 
 
-
 document.addEventListener('input', function() {
-    const serchTerm = searchInput.value.toLocaleLowerCase();
+    const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '' ) {
         resultPlaylist.classList.add('hidden');
         resultArtists.classList.remove('hidden');
         return;
     }
 
-    requestApi( searchTerm);
+    requestApi(searchTerm);
 });
